9ed4d9a254595c0849009dd6f4d70ce29feae912
AKSHIT KUMAR <akskumar3@publicisgroupe.net>
2021-11-02 15:50:26 +0530

    added input button and files

--- taskify/src/App.tsx
+++ taskify/src/App.tsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
-import './App.css';
-import InputField from './components/InputField';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
-const App : React.FC = ()  =>{
-
+const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDOne: false }]);
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo = {todo} setTodo = {setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       
     </div>
   );
-}
+};
 
 export default App;
--- taskify/src/components/InputField.tsx
+++ taskify/src/components/InputField.tsx
@@ -1,31 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import './inputStyle.css'
+import "./inputStyle.css";
 
 interface Props {
-    todo: string;
-    setTodo : React.Dispatch<React.SetStateAction<string>>;
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd :(e:React.FormEvent) => void
 }
 
-const InputField:React.FC<Props> = ({todo , setTodo}) => {
-    return (
-        <form className='input'>
+const InputField: React.FC<Props> = ({ todo, setTodo , handleAdd}) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter your task"
+        className="input_box"
+      />
+      <button className="input_submit" type="submit">
+        Go!
+      </button>
+    </form>
+  );
+};
 
-            <input type="input" 
-            value={todo}
-            onChange={(e) =>
-            setTodo(e.target.value)
-            
-            }
-            
-            
-            
-            placeholder='Enter your task' className='input_box'/>
-            <button className='input_submit' type="submit">Go!</button>
-
-            
-        </form>
-    )
-}
-
-export default InputField
+export default InputField;
