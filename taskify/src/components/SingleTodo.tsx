a69bc41f027ccf66285ecb4c11bb744ce07059e7
AKSHIT KUMAR <akskumar3@publicisgroupe.net>
2021-11-02 20:35:53 +0530

    added css

--- taskify/src/components/SingleTodo.tsx
+++ taskify/src/components/SingleTodo.tsx
@@ -1,10 +1,47 @@
+
 import React from 'react'
 
-const SingleTodo = () => {
+import { Todo } from "../model";
+import {AiFillEdit , AiFillDelete } from 'react-icons/ai'
+import {MdDone} from 'react-icons/md'
+
+import "./inputStyle.css";
+
+
+interface Props{
+
+    todo :Todo;
+    todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+
+    
+}
+const SingleTodo :React.FC<Props> = ({todo, todos, setTodos}) => {
     return (
-        <div>
+        <form className='todos_single'>
+            <span className="todos_single--text">{todo.todo}</span>
+
+            <div>
+
+                <span className="icon">
+                <AiFillEdit/>
+
+                </span>
+                <span className="icon">
+
+                <AiFillDelete/>
+                </span>
+                <span className="icon">
+                   
+                   <MdDone/>
+                </span>
+            </div>
+
+
+
+        </form>
             
-        </div>
+        
     )
 }
 
--- taskify/src/components/TodoList.tsx
+++ taskify/src/components/TodoList.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import "./inputStyle.css";
 
 interface Props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
-  return <div>
-
-      {todos.map(todo => (
-
-                <li>{todo}</li>
-
-      )
-
-
-
-      )}
-  </div>;
+  return (
+    <div>
+      {todos.map((todo) => (
+        <SingleTodo
+          todo={todo}
+          key={todo.id}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TodoList;
